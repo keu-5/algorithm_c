2168a1a0f6a4b9d912601237e65c8a4257c3fc5c
keu-5 <keu02.78@gmail.com>
2023-10-17 18:22:26 +0900

    update c

--- linked.c/circular_linked_list.c
+++ linked.c/circular_linked_list.c
@@ -4,61 +4,26 @@
 struct Node {
     int data;
     struct Node *next;
-} head;
-
-int size() {
-    int i = 0;
-    struct Node *temp = head.next;
-    while (temp!= NULL) {
-        i++;
-        temp = temp->next;
-    }
-    return i;
-}
-
-void insert(int data, int num) {
-    struct Node *newNode;
-    newNode = (struct Node *)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-
-    struct Node *temp = &head;
-
-    if (num < 0 || num > size()) {
-        printf("Invalid input\n");
-        return;
-    } else if (num == 0) {
-        newNode->next = &head;
-        head = *newNode;
-        return;
-    } else {
-        for (int i = 0; i < num; i++) {
-            temp = temp->next;
-        }
-        newNode->data = data;
-        newNode->next = temp->next;
-        temp->next = newNode;
-    }
-}
-
-//NULLが認識されていない?
-void show() {
-    struct Node *temp = &head;
-    while (temp->next != NULL) {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-}
+};
 
 int main(void) {
-    head.data = 0;
-    head.next = NULL;
+    struct Node *head;
+    struct Node *second;
+    struct Node *third; 
 
-    insert(1, 0);
-    insert(2, 1);
-    printf("%d\n", head.data);
-    head = *head.next;
-    printf("%d\n", head.data);
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 1;
+    head->next = second;
+    second->data = 2;
+    second->next = third;
+    third->data = 3;
+    third->next = NULL;
+
+    for (struct Node *p = head; p!= NULL; p = p->next) {
+        printf("%d\n", p->data);
+    }
     return 0;
 }
