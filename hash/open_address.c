a5bca069e906911670a22255b8282ed72648cd00
keu-5 <keu02.78@gmail.com>
2023-10-29 13:08:05 +0900

    update c

--- hash/open_address.c
+++ hash/open_address.c
@@ -19,6 +19,7 @@ int hash(KEY key) {
     return key % BUCKET_SIZE;
 }
 
+//衝突が発生した際に別のバケットに入れるために前回のハッシュ値を用いて再ハッシュを行う
 int reflesh(int h) {
     return (h + 1) % BUCKET_SIZE;
 }
@@ -46,14 +47,17 @@ DATA *find(KEY key) {
         if (k != DELETED && keyequal(key, k)) {
             return &table[h].data;
         }
+        //バケットサイズを超えてしまったら終了
         if (++count >= BUCKET_SIZE) {
             return NULL;
         }
+        //再ハッシュ
         h = reflesh(h);
     }
     return NULL;
 }
 
+//成功したら1,失敗したら0を返す
 int insert(KEY key, DATA *data) {
     int h, count;
     KEY k;
@@ -61,14 +65,18 @@ int insert(KEY key, DATA *data) {
     count = 0;
     h = hash(key);
 
+    //バケットに値が存在している場合
     while ((k = table[h].key) != EMPTY && k != DELETED) {
+        //完全にキーが一致している場合0を返す
         if (keyequal(key, k)) {
             return 0;
         }
+        //バケットサイズを超えてしまったら終了
         if (++count > BUCKET_SIZE) {
             fprintf(stderr, "hash table overflow\n");
             exit(1);
         }
+        //再ハッシュ
         h = reflesh(h);
     }
     table[h].key = key;
@@ -76,6 +84,7 @@ int insert(KEY key, DATA *data) {
     return 1;
 }
 
+//成功したら1,失敗したら0を返す
 int delete(KEY key) {
     int h, count;
     KEY k;
@@ -94,4 +103,66 @@ int delete(KEY key) {
         h = reflesh(h);
     }
     return 0;
+}
+
+int main(void) {
+    int select;
+    KEY key;
+    DATA data;
+
+    while(1) {
+        puts("操作を選んでください");
+        puts("1.insert");
+        puts("2.delete");
+        puts("3.find");
+        puts("4.init");
+        puts("5.exit");
+        scanf("%d", &select);
+
+        switch (select) {
+            case 1:
+                puts("keyを入力してください");
+                scanf("%d", &key);
+                puts("dataを入力してください");
+                scanf("%s", &data);
+
+                if (insert(key, &data) == 1) {
+                    puts("insert success");
+                } else {
+                    puts("insert fail");
+                }
+                break;
+            case 2:
+                puts("keyを入力してください");
+                scanf("%d", &key);
+                if (delete(key) == 1) {
+                    puts("delete success");
+                } else {
+                    puts("delete fail");
+                }
+                break;
+            case 3:
+                puts("keyを入力してください");
+                scanf("%d", &key);
+                DATA *data = find(key);
+
+                if (data == NULL) {
+                    puts("not found");
+                } else {
+                    printf("data = %s\n", data);
+                }
+                break;
+            case 4:
+                init();
+                puts("init success");
+                break;
+            case 5:
+                exit(0);
+                break;
+            default:
+                fprintf(stderr, "invalid select\n");
+                exit(1);
+                break;
+        }
+    }
 }
